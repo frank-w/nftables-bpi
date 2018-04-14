frank-w/nftables-bpi

commit 4176e24e14f0723486253ebcfd2885c77f82f7b1

libnftables: Introduce nft_ctx_set_error()
Analogous to nft_ctx_set_output(), this allows to set a custom file
pointer for writing error messages to.

Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/libnftables.c
@@ -169,6 +169,7 @@ struct nft_ctx *nft_ctx_new(uint32_t flags)
 	init_list_head(&ctx->cache.list);
 	ctx->flags = flags;
 	ctx->output.output_fp = stdout;
+	ctx->output.error_fp = stderr;
 
 	if (flags == NFT_CTX_DEFAULT)
 		nft_ctx_netlink_init(ctx);
@@ -196,6 +197,18 @@ FILE *nft_ctx_set_output(struct nft_ctx *ctx, FILE *fp)
 		return NULL;
 
 	ctx->output.output_fp = fp;
+
+	return old;
+}
+
+FILE *nft_ctx_set_error(struct nft_ctx *ctx, FILE *fp)
+{
+	FILE *old = ctx->output.error_fp;
+
+	if (!fp || ferror(fp))
+		return NULL;
+
+	ctx->output.error_fp = fp;
 
 	return old;
 }
@@ -282,7 +295,6 @@ int nft_run_cmd_from_buffer(struct nft_ctx *nft, char *buf, size_t buflen)
 	LIST_HEAD(msgs);
 	size_t nlbuflen;
 	void *scanner;
-	FILE *fp;
 	char *nlbuf;
 
 	nlbuflen = max(buflen + 1, strlen(buf) + 2);
@@ -297,9 +309,7 @@ int nft_run_cmd_from_buffer(struct nft_ctx *nft, char *buf, size_t buflen)
 	if (nft_run(nft, nft->nf_sock, scanner, &state, &msgs) != 0)
 		rc = -1;
 
-	fp = nft_ctx_set_output(nft, stderr);
 	erec_print_list(&nft->output, &msgs, nft->debug_mask);
-	nft_ctx_set_output(nft, fp);
 	scanner_destroy(scanner);
 	iface_cache_release();
 	free(nlbuf);

include/nftables.h
@@ -14,6 +14,7 @@ struct output_ctx {
 	unsigned int handle;
 	unsigned int echo;
 	FILE *output_fp;
+	FILE *error_fp;
 };
 
 struct nft_cache {

include/nftables/nftables.h
@@ -57,6 +57,8 @@ bool nft_ctx_output_get_echo(struct nft_ctx *ctx);
 void nft_ctx_output_set_echo(struct nft_ctx *ctx, bool val);
 
 FILE *nft_ctx_set_output(struct nft_ctx *ctx, FILE *fp);
+FILE *nft_ctx_set_error(struct nft_ctx *ctx, FILE *fp);
+
 int nft_ctx_add_include_path(struct nft_ctx *ctx, const char *path);
 void nft_ctx_clear_include_paths(struct nft_ctx *ctx);
 

src/erec.c
@@ -124,7 +124,7 @@ void erec_print(struct output_ctx *octx, const struct error_record *erec,
 	unsigned int i, end;
 	int l;
 	off_t orig_offset = 0;
-	FILE *f = octx->output_fp;
+	FILE *f = octx->error_fp;
 
 	if (!f)
 		return;

commit 6e4bbbb278a50e61b6c219680004e7bd4480137c

libnftables: Fix forgotten bit after introducing error_fp
Like nft_run_cmd_from_buffer, nft_run_cmd_from_filename doesn't need to
change output_fp temporarily when printing error records anymore.

Fixes: 4176e24e14f07 ("libnftables: Introduce nft_ctx_set_error()")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/libnftables.c
@@ -433,7 +433,6 @@ int nft_run_cmd_from_filename(struct nft_ctx *nft, const char *filename)
 	LIST_HEAD(msgs);
 	void *scanner;
 	int rc;
-	FILE *fp;
 
 	rc = cache_update(nft->nf_sock, &nft->cache, CMD_INVALID, &msgs,
 			  nft->debug_mask, &nft->output);
@@ -454,9 +453,7 @@ int nft_run_cmd_from_filename(struct nft_ctx *nft, const char *filename)
 	if (nft_run(nft, nft->nf_sock, scanner, &state, &msgs) != 0)
 		rc = -1;
 err:
-	fp = nft_ctx_set_output(nft, stderr);
 	erec_print_list(&nft->output, &msgs, nft->debug_mask);
-	nft_ctx_set_output(nft, fp);
 	scanner_destroy(scanner);
 	iface_cache_release();
 
frank-w/nftables-bpi

commit 9e84f3f083bb33f4de974fd87a5d06fb9609be12

xt: fix build when libxtables is not installed
If libxtables is not even installed, build fails due to to missing
include file.

ifdef LIBXTABLES guard fixes the first error, but results in two
followup failures:
1. missing IFNAMSIZ definition
2. dereference of unknown struct.

Signed-off-by: Florian Westphal <fw@strlen.de>

src/xt.c
@@ -10,7 +10,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <net/if.h>
+#ifdef HAVE_LIBXTABLES
 #include <xtables.h>
+#endif
 #include <getopt.h>
 #include <ctype.h>	/* for isspace */
 #include <statement.h>
@@ -76,6 +79,7 @@ void xt_stmt_xlate(const struct stmt *stmt, struct output_ctx *octx)
 
 void xt_stmt_release(const struct stmt *stmt)
 {
+#ifdef HAVE_LIBXTABLES
 	switch (stmt->xt.type) {
 	case NFT_XT_MATCH:
 		if (!stmt->xt.match)
@@ -95,6 +99,7 @@ void xt_stmt_release(const struct stmt *stmt)
 	default:
 		break;
 	}
+#endif
 	xfree(stmt->xt.entry);
 }
 

commit 1e224c1399236cb5f93b53b4cebe7944237ad204

xt: fix build with --with-xtables
The previous change is bonkers, it fixes build when libxtables isn't
even installed, but broke build when --with-xtables is provided to configure.

Reason is that the include guard comes too early, so xtables.h is never
included, causing build to fail because no libxtables function prototypes
and definitions are available.

Fixes: 9e84f3f083bb ("xt: fix build when libxtables is not installed")
Signed-off-by: Florian Westphal <fw@strlen.de>

src/xt.c
@@ -11,9 +11,6 @@
 #include <time.h>
 #include <string.h>
 #include <net/if.h>
-#ifdef HAVE_LIBXTABLES
-#include <xtables.h>
-#endif
 #include <getopt.h>
 #include <ctype.h>	/* for isspace */
 #include <statement.h>
@@ -28,6 +25,10 @@
 #include <linux/netfilter_ipv6/ip6_tables.h>
 #include <linux/netfilter_arp/arp_tables.h>
 #include <linux/netfilter_bridge/ebtables.h>
+
+#ifdef HAVE_LIBXTABLES
+#include <xtables.h>
+#endif
 
 void xt_stmt_xlate(const struct stmt *stmt, struct output_ctx *octx)
 {
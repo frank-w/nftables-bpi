frank-w/nftables-bpi

commit 88456a7ef011728a98c950447630f0a128dcad13

rule: list only the table containing object
For e.g.

 % nft list ct helper ip raw cthelp1
 table ip filter {
 }
 table ip raw {
	ct helper cthelp1 {
		type "ftp" protocol tcp
		l3proto ip
	}
 }

With this patch, print only table raw.

Signed-off-by: Harsha Sharma <harshasharmaiitr@gmail.com>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -1593,15 +1593,13 @@ static int do_list_obj(struct netlink_ctx *ctx, struct cmd *cmd, uint32_t type)
 		    cmd->handle.family != table->handle.family)
 			continue;
 
-		nft_print(ctx->octx, "table %s %s {\n",
-			  family2str(table->handle.family),
-			  table->handle.table.name);
-
 		if (cmd->handle.table.name != NULL &&
-		    strcmp(cmd->handle.table.name, table->handle.table.name)) {
-			nft_print(ctx->octx, "}\n");
+		    !strcmp(cmd->handle.table.name, table->handle.table.name)) {
+			nft_print(ctx->octx, "table %s %s {\n",
+				  family2str(table->handle.family),
+				  cmd->handle.table.name);
+		} else
 			continue;
-		}
 
 		list_for_each_entry(obj, &table->objs, list) {
 			if (obj->type != type ||

commit dafac7d528de0890b82f943fc818ccb7a82fca7d

rule: fix object listing when no table is given
'nft list quotas' would not print anything at all anymore.

Fixes: 88456a7ef01172 ("rule: list only the table containing object")
Signed-off-by: Florian Westphal <fw@strlen.de>

src/rule.c
@@ -1946,12 +1946,15 @@ static int do_list_obj(struct netlink_ctx *ctx, struct cmd *cmd, uint32_t type)
 			continue;
 
 		if (cmd->handle.table.name != NULL &&
-		    !strcmp(cmd->handle.table.name, table->handle.table.name)) {
-			nft_print(&ctx->nft->output, "table %s %s {\n",
-				  family2str(table->handle.family),
-				  cmd->handle.table.name);
-		} else
+		    strcmp(cmd->handle.table.name, table->handle.table.name))
 			continue;
+
+		if (list_empty(&table->objs))
+			continue;
+
+		nft_print(&ctx->nft->output, "table %s %s {\n",
+			  family2str(table->handle.family),
+			  table->handle.table.name);
 
 		list_for_each_entry(obj, &table->objs, list) {
 			if (obj->type != type ||

tests/shell/testcases/listing/0014objects_0
@@ -15,6 +15,13 @@ $NFT add quota test https-quota 25 mbytes
 $NFT add ct helper test cthelp { type \"sip\" protocol tcp \; }
 $NFT add table test-ip
 
+GET="$($NFT list quotas)"
+if [ "$EXPECTED" != "$GET" ] ; then
+	DIFF="$(which diff)"
+	[ -x $DIFF ] && $DIFF -u <(echo "$EXPECTED") <(echo "$GET")
+	exit 1
+fi
+
 GET="$($NFT list quota test https-quota)"
 if [ "$EXPECTED" != "$GET" ] ; then
 	DIFF="$(which diff)"
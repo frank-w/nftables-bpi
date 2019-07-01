43ae7a48ae3deacf73a7a50e60f6809eed9b3f59
Pablo Neira Ayuso <pablo@netfilter.org>
2019-07-01 19:54:00 +0200

    rule: do not print semicolon in ct timeout
    table ip filter {
            ct timeout agressive-tcp {
                    protocol tcp;
                                ^--- remove this semicolon
    
    Not needed, remove it.
    
    Fixes: c7c94802679c ("src: add ct timeout support")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/rule.c
+++ src/rule.c
@@ -1834,7 +1834,7 @@ static void obj_print_data(const struct obj *obj,
 		nft_print(octx, "%s", opts->nl);
 		nft_print(octx, "%s%sprotocol ", opts->tab, opts->tab);
 		print_proto_name_proto(obj->ct_timeout.l4proto, octx);
-		nft_print(octx, ";%s", opts->nl);
+		nft_print(octx, "%s", opts->nl);
 		nft_print(octx, "%s%sl3proto %s%s",
 			  opts->tab, opts->tab,
 			  family2str(obj->ct_timeout.l3proto),

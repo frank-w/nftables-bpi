a4ec053812610400b7a9e6c060d8b7589dedd5b1
Pablo Neira Ayuso <pablo@netfilter.org>
2019-10-09 11:54:32 +0200

    segtree: always close interval in non-anonymous sets
    Skip this optimization for non-anonymous sets, otherwise, element
    deletion breaks.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/segtree.c
+++ src/segtree.c
@@ -496,7 +496,8 @@ static void segtree_linearize(struct list_head *list, const struct set *set,
 			 * (prev_right, ei_left).
 			 */
 			mpz_add_ui(p, prev->right, 1);
-			if (mpz_cmp(p, ei->left) < 0) {
+			if (mpz_cmp(p, ei->left) < 0 ||
+			    !(set->flags & NFT_SET_ANONYMOUS)) {
 				mpz_sub_ui(q, ei->left, 1);
 				nei = ei_alloc(p, q, NULL, EI_F_INTERVAL_END);
 				list_add_tail(&nei->list, list);

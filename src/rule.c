frank-w/nftables-bpi

commit 43ae7a48ae3deacf73a7a50e60f6809eed9b3f59

rule: do not print semicolon in ct timeout
table ip filter {
        ct timeout agressive-tcp {
                protocol tcp;
                            ^--- remove this semicolon

Not needed, remove it.

Fixes: c7c94802679c ("src: add ct timeout support")
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -1834,7 +1834,7 @@ static void obj_print_data(const struct obj *obj,
 		nft_print(octx, "%s", opts->nl);
 		nft_print(octx, "%s%sprotocol ", opts->tab, opts->tab);
 		print_proto_name_proto(obj->ct_timeout.l4proto, octx);
-		nft_print(octx, ";%s", opts->nl);
+		nft_print(octx, "%s", opts->nl);
 		nft_print(octx, "%s%sl3proto %s%s",
 			  opts->tab, opts->tab,
 			  family2str(obj->ct_timeout.l3proto),

tests/shell/testcases/listing/0013objects_0
@@ -13,7 +13,7 @@ EXPECTED="table ip test {
 	}
 
 	ct timeout cttime {
-		protocol udp;
+		protocol udp
 		l3proto ip
 		policy = { unreplied: 15, replied: 12 }
 	}

tests/shell/testcases/nft-f/0017ct_timeout_obj_0
@@ -2,7 +2,7 @@
 
 EXPECTED='table ip filter {
 	ct timeout cttime{
-		protocol tcp;
+		protocol tcp
 		l3proto ip
 		policy = { established: 123, close: 12 }
 	}

tests/shell/testcases/nft-f/dumps/0017ct_timeout_obj_0.nft
@@ -1,6 +1,6 @@
 table ip filter {
 	ct timeout cttime {
-		protocol tcp;
+		protocol tcp
 		l3proto ip
 		policy = { established: 123, close: 12 }
 	}

commit fb050d7799abfd6bda6c21ad78ed5d9d2c92132f

rule: Fix for single line ct timeout printing
Commit 43ae7a48ae3de ("rule: do not print semicolon in ct timeout")
removed an extra semicolon at end of line, but thereby broke single line
output. The correct fix is to use opts->stmt_separator which holds
either newline or semicolon chars depending on output mode.

Fixes: 43ae7a48ae3de ("rule: do not print semicolon in ct timeout")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Acked-by: Florian Westphal <fw@strlen.de>

src/rule.c
@@ -1869,7 +1869,7 @@ static void obj_print_data(const struct obj *obj,
 		nft_print(octx, "%s", opts->nl);
 		nft_print(octx, "%s%sprotocol ", opts->tab, opts->tab);
 		print_proto_name_proto(obj->ct_timeout.l4proto, octx);
-		nft_print(octx, "%s", opts->nl);
+		nft_print(octx, "%s", opts->stmt_separator);
 		nft_print(octx, "%s%sl3proto %s%s",
 			  opts->tab, opts->tab,
 			  family2str(obj->ct_timeout.l3proto),
frank-w/nftables-bpi

commit dd44081d91ceaead0a9730f1ab5c8041a4d905e2

segtree: Fix add and delete of element in same batch
The commit this fixes accidentally broke a rather exotic use-case which
is but used in set-simple.t of tests/monitor:

| # nft 'add element t s { 22-25 }; delete element t s { 22-25 }'

Since ranges are now checked for existence in userspace before delete
command is submitted to kernel, the second command above was rejected
because the range in question wasn't present in cache yet. Fix this by
adding new interval set elements to cache after creating the batch job
for them.

Fixes: decc12ec2dc31 ("segtree: Check ranges when deleting elements")
Signed-off-by: Phil Sutter <phil@nwl.cc>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -1511,6 +1511,14 @@ static int __do_add_setelems(struct netlink_ctx *ctx, struct set *set,
 	if (mnl_nft_setelem_add(ctx, set, expr, flags) < 0)
 		return -1;
 
+	if (set->init != NULL &&
+	    set->flags & NFT_SET_INTERVAL) {
+		interval_map_decompose(expr);
+		list_splice_tail_init(&expr->expressions, &set->init->expressions);
+		set->init->size += expr->size;
+		expr->size = 0;
+	}
+
 	return 0;
 }
 

commit b6d50bfde21b5a24a606cbf22137e04e8e0f195d

rule: memleak in __do_add_setelems()
This patch invokes interval_map_decompose() with named sets:

==3402== 2,352 (128 direct, 2,224 indirect) bytes in 1 blocks are definitely lost in loss record 9 of 9
==3402==    at 0x483577F: malloc (vg_replace_malloc.c:299)
==3402==    by 0x48996A8: xmalloc (utils.c:36)
==3402==    by 0x4899778: xzalloc (utils.c:65)
==3402==    by 0x487CB46: expr_alloc (expression.c:45)
==3402==    by 0x487E2A0: mapping_expr_alloc (expression.c:1140)
==3402==    by 0x4898AA8: interval_map_decompose (segtree.c:1095)
==3402==    by 0x4872BDF: __do_add_setelems (rule.c:1569)
==3402==    by 0x4872BDF: __do_add_setelems (rule.c:1559)
==3402==    by 0x4877936: do_command (rule.c:2710)
==3402==    by 0x489F1CB: nft_netlink.isra.5 (libnftables.c:42)
==3402==    by 0x489FB07: nft_run_cmd_from_filename (libnftables.c:508)
==3402==    by 0x10A9AA: main (main.c:455)

Fixes: dd44081d91ce ("segtree: Fix add and delete of element in same batch")
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -1563,7 +1563,8 @@ static int __do_add_setelems(struct netlink_ctx *ctx, struct set *set,
 	if (mnl_nft_setelem_add(ctx, set, expr, flags) < 0)
 		return -1;
 
-	if (set->init != NULL &&
+	if (!set_is_anonymous(set->flags) &&
+	    set->init != NULL &&
 	    set->flags & NFT_SET_INTERVAL &&
 	    set->desc.field_count <= 1) {
 		interval_map_decompose(expr);